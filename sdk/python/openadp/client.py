2364e018ab0d470a377643d15372d02e9b0a9433
Bill Cox <waywardgeek@gmail.com>
2025-06-21 22:46:42 -0700

    Minor fix.

--- sdk/python/openadp/client.py
+++ sdk/python/openadp/client.py
@@ -28,7 +28,6 @@ try:
     from cryptography.hazmat.primitives import hashes, serialization
     from cryptography.hazmat.primitives.kdf.hkdf import HKDF  
     from cryptography.hazmat.primitives.asymmetric import x25519
-    from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
     from cryptography.hazmat.backends import default_backend
     CRYPTO_AVAILABLE = True
 except ImportError:
