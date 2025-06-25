8697f0b2327a521d5f023c0c6d665ac357c90396
Bill Cox <waywardgeek@gmail.com>
2025-06-24 17:58:02 -0700

    Some edits for publishing to pypi, npn, and pkg.go.dev.

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -45,7 +45,7 @@ def read_requirements():
 
 setup(
     name="openadp",
-    version="1.0.0",
+    version="0.1.1",
     author="OpenADP Team",
     author_email="contact@openadp.org",
     description="OpenADP Python SDK for distributed secret sharing and advanced data protection",
